2b3727463c2a7e234e89f50db90c653c647ccb5d
Sho Ishida <isshi0417@gmail.com>
2024-05-14 19:16:49 +0900

    Complete 'Madlibs'

---
+++ easy_3_files/madlibs.py
@@ -0,0 +1,8 @@
+noun = input('Enter a noun: ')
+verb = input('Enter a verb: ')
+adjective = input('Enter an adjective: ')
+adverb = input('Enter an adverb: ')
+
+print(f'Do you walk your {adjective} {noun} {adverb}? That\'s hilarious!')
+print(f'The {adjective} {noun} {verb}s {adjective} over the lazy dog.')
+print(f'The {noun} {adjective} {verb}s up to Joe\'s blue turtle.')
